4edb97b45b8815e3af1ebefc4eccff349d464697
Stian Laate <stian.late@visma.com>
2019-07-15 14:23:11 +0200

    added java backend starter structure with spring initializr

---
+++ backend/rpi-greenhouse/src/main/java/com/greenhouse/rpigreenhouse/RpiGreenhouseApplication.java
@@ -0,0 +1,13 @@
+package com.greenhouse.rpigreenhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiGreenhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpiGreenhouseApplication.class, args);
+	}
+
+}
---
+++ backend/rpi-greenhouse/src/test/java/com/greenhouse/rpigreenhouse/RpiGreenhouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenhouse.rpigreenhouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RpiGreenhouseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
