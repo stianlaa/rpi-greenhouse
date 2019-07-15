917e58cb4a6cc08e5fea70d1dca7199076720b72
Stian Laate <stian.late@visma.com>
2019-07-15 17:56:02 +0200

    nginx testing

--- backend/rpi-greenhouse/src/main/java/com/greenhouse/rpigreenhouse/RpiGreenhouseApplication.java
+++ backend/rpi-greenhouse/src/main/java/com/greenhouse/rpigreenhouse/RpiGreenhouseApplication.java
@@ -2,6 +2,8 @@ package com.greenhouse.rpigreenhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RpiGreenhouseApplication {
@@ -10,4 +12,13 @@ public class RpiGreenhouseApplication {
 		SpringApplication.run(RpiGreenhouseApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloNginx {
+
+	@RequestMapping("/")
+	String index() {
+		return "This is a test for nginx deployment";
+	}
 }
